8b5bf697138fac53b612ed4cb219959dae92a042
Fell1pe <fellipe.lucas.zanin@gmail.com>
2024-04-19 10:04:14 -0300

    adiçãoes de preenchimento de campos e esqueci-senha com index2

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,32 @@
 const mode = document.getElementById('mode_icon');
 
 mode.addEventListener('click', () => {
-    const form = document.getElementById('login_form');
-    if(mode.classList.contains('fa-moon')){
-        mode.classList.remove('fa-moon');
-        mode.classList.add('fa-sun');
+  const form = document.getElementById('login_form');
+  if (mode.classList.contains('fa-moon')) {
+    mode.classList.remove('fa-moon');
+    mode.classList.add('fa-sun');
+    form.classList.add('dark');
+    return;
+  }
 
-        form.classList.add('dark')
-        return
-    }
+  mode.classList.add('fa-moon');
+  mode.classList.remove('fa-sun');
+  form.classList.remove('dark');
+});
 
-    mode.classList.add('fa-moon');
-    mode.classList.remove('fa-sun');
-    form.classList.remove('dark')
+const nameInput = document.getElementById('name');
+const emailInput = document.getElementById('Email');
+const passwordInput = document.getElementById('password');
+
+nameInput.setAttribute('required', true);
+emailInput.setAttribute('required', true);
+passwordInput.setAttribute('required', true);
+
+const loginForm = document.getElementById('login_form');
+
+loginForm.addEventListener('submit', (event) => {
+  if (nameInput.value === '' || emailInput.value === '' || passwordInput.value === '') {
+    event.preventDefault(); 
+    alert('Por favor, preencha todos os campos obrigatórios.');
+  }
 });
